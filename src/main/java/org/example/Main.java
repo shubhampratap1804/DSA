568511db8531a72f7a7467a789d28fbdcb0a85c7
Shubham <shubham@Shubhams-MacBook-Pro.local>
2024-11-23 21:29:00 +0530

    added: program to find product of contagious sub arrays

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,40 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] arr = {1,2,3,4,5,6,7,8,9};
-        numbersNotDivisibleByTwoAndThree(arr);
+        int[] arr = {2, 3, -2, 4, 5};
+        int length = 5;
+
+        maximumProductOfSubArray(length, arr);
     }
 
+    /*program to find the maximum product of sub-array*/
+    static void maximumProductOfSubArray(int length, int[] arr){
+        
+        int maxProduct = arr[0];
+        int minProduct = arr[0];
+        int result = arr[0];
+
+        for (int j : arr) {
+            if (j < 0) {
+                int temp = maxProduct;
+                maxProduct = minProduct;
+                minProduct = temp;
+            }
+
+            maxProduct = Math.max(j, maxProduct * j);
+            minProduct = Math.min(j, minProduct * j);
+
+            result = Math.max(result, maxProduct);
+        }
+System.out.println("Maximum product of sub-array is : " +result);
+
+    }
+
+    /*program to find numbers in an array that ar not divisible by either 2 or 3 or both
     static void numbersNotDivisibleByTwoAndThree(int[] arr){
         List<Integer> resultSet = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
@@ -22,7 +47,7 @@ public class Main {
         if(!resultSet.isEmpty()){
             System.out.println(resultSet.toString());
         }else {
-            System.out.println("Every number is divisible by 2 and 3");
+            System.out.println("Every number is divisible by either 2 or 3 or both.");
         }
-    }
+    }*/
 }
